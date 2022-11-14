816fea46b6fa34cc1006e2f058f0052ae95a68ad
amaiaita <amaia.imazblanco1@nhs.net>
2022-11-14 15:17:31 +0000

    complete task 5

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require("express");
-const { getArticles } = require("./controllers/articles");
+const { getArticles, getArticleByID } = require("./controllers/articles");
 const { getTopics } = require("./controllers/topics");
 const app = express();
 
 app.get("/api/topics", getTopics);
 app.get("/api/articles", getArticles);
+app.get("/api/articles/:article_id", getArticleByID);
+
+app.use((err, req, res, next) => {
+  if (err.msg && err.status) {
+    res.status(err.status).send({ msg: err.msg });
+  }
+});
 
 module.exports = app;
