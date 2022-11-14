4293365305a7ddfe2012c80c6f7fdc2ee911d343
amaiaita <amaia.imazblanco1@nhs.net>
2022-11-14 14:42:27 +0000

    remove express.json()

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ const { getArticles } = require("./controllers/articles");
 const { getTopics } = require("./controllers/topics");
 const app = express();
 
-app.use(express.json());
-
 app.get("/api/topics", getTopics);
 app.get("/api/articles", getArticles);
 
