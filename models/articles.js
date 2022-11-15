fff2241381732a64d81c804d1fa8b5d247864cd0
amaiaita <amaia.imazblanco1@nhs.net>
2022-11-15 10:46:19 +0000

    small changes from feedback

--- models/articles.js
+++ models/articles.js
@@ -7,7 +7,7 @@ exports.obtainArticles = () => {
       `   
         SELECT articles.author, articles.title, articles.article_id, topic, articles.created_at, articles.votes, COUNT(comment_id) AS comment_count
         FROM articles
-        JOIN comments ON comments.article_id=articles.article_id
+        LEFT JOIN comments ON comments.article_id=articles.article_id
         GROUP BY articles.article_id
         ORDER BY articles.created_at DESC;
     `
