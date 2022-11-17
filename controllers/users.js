191f36423de9fa698cdaf93a32973939068edc40
amaiaita <114224821+amaiaita@users.noreply.github.com>
2022-11-17 12:20:47 +0000

    complete ticket 17 (#16)
    Co-authored-by: amaiaita <amaia.imazblanco1@nhs.net>

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,16 @@
-const { obtainUsers } = require("../models/users");
+const { obtainUsers, obtainUserByUsername } = require("../models/users");
 
 exports.getUsers = (req, res, next) => {
   obtainUsers().then((users) => {
     res.status(200).send({ users });
   });
+};
+
+exports.getUserByUsername = (req, res, next) => {
+  const { username } = req.params;
+  obtainUserByUsername(username)
+    .then((user) => {
+      res.status(200).send({ user });
+    })
+    .catch((err) => next(err));
 };
