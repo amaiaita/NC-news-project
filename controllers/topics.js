44c58d6bbdc3a8157debcc50ab7464da533d0802
amaiaita <amaia.imazblanco1@nhs.net>
2022-11-18 10:30:48 +0000

    complete post topic

--- controllers/topics.js
+++ controllers/topics.js
@@ -1,7 +1,15 @@
-const { obtainTopics } = require("../models/topics");
+const { obtainTopics, addTopic } = require("../models/topics");
 
 exports.getTopics = (req, res, next) => {
   obtainTopics().then((topics) => {
     res.status(200).send({ topics });
+  });
+};
+
+exports.postTopic = (req, res, next) => {
+  const topic = req.body;
+  addTopic(topic).then((topic) => {
+    console.log(topic);
+    res.status(201).send({ topic });
   });
 };
